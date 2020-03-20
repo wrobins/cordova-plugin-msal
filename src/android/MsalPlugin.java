a394c346214f537b5839decf90794767473c5ce3
wrobins <wrobins@kent.edu>
2020-03-20 14:36:23 -0400

    Enabled authorizationUserAgent config option.

--- src/android/MsalPlugin.java
+++ src/android/MsalPlugin.java
@@ -131,8 +131,7 @@ public class MsalPlugin extends CordovaPlugin {
                         data = "{\n" +
                                 "    \"client_id\" : \"" + MsalPlugin.this.clientId + "\",\n" +
                                 "    \"account_mode\": \"" + options.getString("accountMode") + "\",\n" +
-                                // TODO: Make WEBVIEW dynamic after OutSystems branch: options.getString("authorizationUserAgent")
-                                "    \"authorization_user_agent\" : \"WEBVIEW\",\n" +
+                                "    \"authorization_user_agent\" : \"" + options.getString("authorizationUserAgent") + "\",\n" +
                                 "    \"redirect_uri\" : \"msauth://" + MsalPlugin.this.activity.getApplicationContext().getPackageName() + "/" + keyHashUrlFriendly + "\",\n" +
                                 "    \"multiple_clouds_supported\": " + options.getBoolean("multipleCloudsSupported") + ",\n" +
                                 "    \"broker_redirect_uri_registered\": " + options.getBoolean("brokerRedirectUri") + ",\n" +
