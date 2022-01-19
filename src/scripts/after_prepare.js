64b7336004feb97ca45865dc15a1e28a620c6207
Maurice Schoenmakers <maurice.schoenmakerrs@gmail.com>
2022-01-19 10:12:28 +0100

    improved hook script and add platform_www change to allow serve

--- src/scripts/after_prepare.js
+++ src/scripts/after_prepare.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('platforms/browser/browser.json').toString());
-const CLIENT_ID = `'${config.installed_plugins["cordova-plugin-msal"].CLIENT_ID}'`;
-const TENANT_ID = `'${config.installed_plugins["cordova-plugin-msal"].TENANT_ID}'`;
+const browser_platform='platforms/browser/browser.json';
 
 // If our application has the browser platform installed, this is ideally where we want to configure it.
-if (fs.existsSync('platforms/browser/browser.json')) {
-    const proxyFile = 'platforms/browser/www/plugins/cordova-plugin-msal/src/browser/MsalProxy.js';
-    const contents = fs.readFileSync(proxyFile).toString();
-    fs.writeFileSync(proxyFile, contents.replace(/CLIENT_ID/g, CLIENT_ID).replace(/TENANT_ID/g, TENANT_ID));
+if (fs.existsSync(browser_platform)) {
+
+    const config = JSON.parse(fs.readFileSync(browser_platform).toString());
+    const CLIENT_ID = `'${config.installed_plugins["cordova-plugin-msal"].CLIENT_ID}'`;
+    const TENANT_ID = `'${config.installed_plugins["cordova-plugin-msal"].TENANT_ID}'`;
+
+    [
+        'platforms/browser/www/plugins/cordova-plugin-msal/src/browser/MsalProxy.js',
+        'platforms/browser/platform_www/plugins/cordova-plugin-msal/src/browser/MsalProxy.js'
+
+    ].forEach( file => {
+        if (fs.existsSync(file)) {
+            const contents = fs.readFileSync(file).toString();
+            fs.writeFileSync(file, contents.replace(/CLIENT_ID/g, CLIENT_ID).replace(/TENANT_ID/g, TENANT_ID));
+        }
+    });
 }
